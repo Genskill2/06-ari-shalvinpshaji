29a6eb2ad55665b59382896a2dd5793b10966e70
Shalvin P Shaji <shalvinpshaji@gmail.com>
2021-06-30 15:03:29 +0530

    Contracted solution into single function

--- ari.c
+++ ari.c
@@ -3,56 +3,24 @@
 #include <string.h>
 #include <math.h>
 
-char a[20];
+char* ari(char* s){
 
-int num_alpha(char*);
-int num_sentences(char*);
-int num_words(char*);
-char* ari(char*);
+    int count_alpha = 0;
+    int count_word = 0;
+    int count_sentence = 0;
 
-/* int main(){
-    char s[2048];
-    fgets(s, 2048, stdin);
-    char* x = ari(s);
-    printf("%s \n", x);
-    return 0;
-
-} */
-
-int num_alpha(char* s){
-    int count = 0;
     for (int i=0;i< strlen(s); i++){
         if (isalnum(s[i])){
-            count += 1;
+            count_alpha += 1;
+        }
+        else if (s[i] == '!' || s[i] == '.' || s[i] == '?'){
+            count_sentence += 1;
+        }
+        else if (s[i] == ' '){
+            count_word += 1;
         }
     }
-    return count;
-}
-
-int num_sentences(char* s){
-    int count = 0;
-    for (int i=0; i< strlen(s); i++){
-        if (s[i] == '!' || s[i] == '.' || s[i] == '?'){
-            count  += 1;
-        }  
-    }
-    return count;
-}
-int num_words(char* s){
-    int count = 0;
-    for (int i =0;i< strlen(s); i++){
-        if (s[i] == ' '){
-            count += 1;
-        }
-    }
-    return count;
-}
-
-char* ari(char* s){
-    int alpha = num_alpha(s);
-    int sen = num_sentences(s);
-    int words = num_words(s);
-    int score = ceil(4.71 * alpha/words + 0.5 * words/sen - 21.43);
+    int score = ceil(4.71 * count_alpha/count_word + 0.5 * count_word/count_sentence - 21.43);
     if (score > 14){
         score = 14;
     }
